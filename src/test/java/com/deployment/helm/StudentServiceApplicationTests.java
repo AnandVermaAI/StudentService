53a75132499684a5f264255dfe2fdaeb58c4796e
anandverma120691 <verma.av1991@gmail.com>
2025-10-06 12:20:36 +0530

    po

---
+++ src/test/java/com/deployment/helm/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployment.helm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
